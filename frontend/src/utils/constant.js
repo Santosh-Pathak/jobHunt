b4812a2d233d44678de49e645a2ab4d583b1ecba
Santosh Pathak <106902202+Santosh-Pathak@users.noreply.github.com>
2025-06-20 11:23:09 +0530

    FrontEnd/Client Side Project Setup Completed Succesfully

---
+++ frontend/src/utils/constant.js
@@ -0,0 +1,5 @@
+export const USER_API_END_POINT = `${BASE_URL}/user`;
+export const JOB_API_END_POINT = `${BASE_URL}/job`;
+export const APPLICATION_API_END_POINT = `${BASE_URL}/application`;
+export const COMPANY_API_END_POINT = `${BASE_URL}/company`;
+export const CHATBOT_API_END_POINT = `${BASE_URL}/chatboat/ask`;
